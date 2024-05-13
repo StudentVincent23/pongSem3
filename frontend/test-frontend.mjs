cc77ee4555fc6bef88a7c13101cf77de050b266e
StudentVincent23 <162105257+StudentVincent23@users.noreply.github.com>
2024-05-13 16:41:06 +0200

    remove the button

--- frontend/test-frontend.mjs
+++ frontend/test-frontend.mjs
@@ -3,12 +3,3 @@ import test from 'ava';
 test("Hellow world! from frontend", t => {
     t.pass();
 });
-
-test('button click calls myMethod', async t => {
-    const wrapper = shallowMount(MyComponent);
-    const button = wrapper.find('button');
-    await button.trigger('click');
-    t.true(wrapper.vm.myMethod.calledOnce);
-});
-
-          

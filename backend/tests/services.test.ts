ce3db505b77ef19e166b85a2476cfb93f96dcc06
StudentVincent23 <162105257+StudentVincent23@users.noreply.github.com>
2024-06-11 13:40:27 +0200

    new database

--- backend/tests/services.test.ts
+++ backend/tests/services.test.ts
@@ -2,8 +2,6 @@ import { describe, expect, it } from 'vitest';
 import { AddUser, GetUsers, DeleteUserById } from '../services';
 
 describe('User Services', () => {
-    
-
     const name = 'testUser06/06/2024';
 
     it('add, delete and get user test', async () => {

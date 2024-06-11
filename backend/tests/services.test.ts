cca7a2b13b659aa47bcdd537025d0dc5a8376c54
StudentVincent23 <162105257+StudentVincent23@users.noreply.github.com>
2024-06-11 13:38:01 +0200

    backends tests back

--- backend/tests/services.test.ts
+++ backend/tests/services.test.ts
@@ -4,30 +4,29 @@ import { AddUser, GetUsers, DeleteUserById } from '../services';
 describe('User Services', () => {
     
 
-    // const name = 'testUser06/06/2024';
+    const name = 'testUser06/06/2024';
 
     it('add, delete and get user test', async () => {
-        expect(true).toBe(true);
-    //     const score = 100;
-    //     const usersCleanUp = await GetUsers();
-    //     const foundUserCleanUp = usersCleanUp.find(user => user.name === name);
+        const score = 100;
+        const usersCleanUp = await GetUsers();
+        const foundUserCleanUp = usersCleanUp.find(user => user.name === name);
 
-    //     if (foundUserCleanUp?.name === name) {
-    //         expect(foundUserCleanUp?.name).toBe(name);
-    //         DeleteUserById(foundUserCleanUp.id);
-    //     } 
+        if (foundUserCleanUp?.name === name) {
+            expect(foundUserCleanUp?.name).toBe(name);
+            DeleteUserById(foundUserCleanUp.id);
+        } 
 
-    //     AddUser(name, score); 
+        AddUser(name, score); 
 
-    //     const users = await GetUsers();
-    //     const foundUser = users.find(user => user.name === name);
+        const users = await GetUsers();
+        const foundUser = users.find(user => user.name === name);
 
-    //     if (foundUser?.name === name) {
-    //         expect(foundUser?.name).toBe(name);
-    //         expect(foundUser?.score).toBe(score);
-    //     } else {
-    //         // throw new Error('User not found not added');
-    //     }  
+        if (foundUser?.name === name) {
+            expect(foundUser?.name).toBe(name);
+            expect(foundUser?.score).toBe(score);
+        } else {
+            // throw new Error('User not found not added');
+        }  
     });
 });
      
